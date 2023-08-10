7e7c0a329aa48dd240b6e76ed992724c500e9382
SheepMed <141952433+SheepMed@users.noreply.github.com>
2023-08-10 19:27:28 +0100

    Add files via upload

--- autoclicker.py
+++ autoclicker.py
@@ -1,3 +1,4 @@
+import threading
 import tkinter as tk
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
@@ -27,24 +28,26 @@ class AutoClickerApp:
         self.listener.start()
 
         self.clicking = False  # To keep track of the auto-clicking loop
-        self.auto_click_task = None  # To store the auto-clicking task ID
+        self.auto_click_thread = None  # Thread for auto-clicking
 
     def toggle_clicking(self):
         self.running = not self.running
         if self.running:
             self.start_stop_button.config(text="Stop Clicking")
             self.clicking = True
-            self.auto_click()
+            self.auto_click_thread = threading.Thread(target=self.auto_click)
+            self.auto_click_thread.start()
         else:
             self.start_stop_button.config(text="Start Clicking")
             self.clicking = False
+            if self.auto_click_thread:
+                self.auto_click_thread.join()
+                self.auto_click_thread = None
 
     def auto_click(self):
-        if self.clicking:
+        while self.clicking:
             self.mouse.click(self.button)
-            self.auto_click_task = self.root.after(int(self.delay * 1000), self.auto_click)
-        else:
-            self.auto_click_task = None
+            time.sleep(self.delay)
 
     def on_key_press(self, key):
         if key == self.start_stop_key:
@@ -52,19 +55,21 @@ class AutoClickerApp:
         elif key == self.stop_key:
             self.running = False
             self.clicking = False
-            if self.auto_click_task is not None:
-                self.root.after_cancel(self.auto_click_task)
+            if self.auto_click_thread:
+                self.auto_click_thread.join()
+                self.auto_click_thread = None
         elif key == self.hotkey:
             if self.running:
                 self.running = False
                 self.clicking = False
-                if self.auto_click_task is not None:
-                    self.root.after_cancel(self.auto_click_task)
+                if self.auto_click_thread:
+                    self.auto_click_thread.join()
+                    self.auto_click_thread = None
 
     def exit_program(self):
         self.running = False
-        if self.auto_click_task is not None:
-            self.root.after_cancel(self.auto_click_task)
+        if self.auto_click_thread:
+            self.auto_click_thread.join()
         self.listener.stop()
         self.root.destroy()
 
