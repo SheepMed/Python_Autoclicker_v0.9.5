e7f231cdd0e7d966db05b9e730abc7d32c13546e
SheepMed <141952433+SheepMed@users.noreply.github.com>
2023-08-10 20:09:16 +0100

    Add files via upload

--- autoclicker.py
+++ autoclicker.py
@@ -5,30 +5,38 @@ from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
 import time
 
-class AutoClickerApp:
+class SheepMedAutoClicker:
     def __init__(self, root):
         self.root = root
-        self.root.title("Auto Clicker")
+        self.root.title("SheepMed Auto Clicker")
+        self.root.geometry("300x300")  
 
-        # Initialize controller, default values, and hotkey
+       
         self.mouse = Controller()
         self.delay = 0.001
         self.button = Button.right
         self.hotkey = KeyCode(char='c')
 
-        # Define keyboard keys
+        
         self.start_stop_key = KeyCode(char='a')
         self.stop_key = KeyCode(char='b')
 
-        # Create GUI components
-        self.start_stop_button = tk.Button(self.root, text="Start Clicking", command=self.toggle_clicking)
-        self.change_hotkey_button = tk.Button(self.root, text="Change Hotkey", command=self.change_hotkey)
-        self.change_delay_button = tk.Button(self.root, text="Change Delay", command=self.change_delay)
-        self.hotkey_label = tk.Label(self.root, text="Hotkey: {}".format(self.hotkey.char))
-        self.delay_label = tk.Label(self.root, text="Current Delay: {}".format(self.delay))
-        self.exit_button = tk.Button(self.root, text="Exit", command=self.exit_program)
+        
+        self.start_stop_button = tk.Button(self.root, text="Start Clicking", command=self.toggle_clicking, height=2, width=20)
+        self.change_hotkey_button = tk.Button(self.root, text="Change Hotkey", command=self.change_hotkey, height=2, width=20)
+        self.change_delay_button = tk.Button(self.root, text="Change Delay", command=self.change_delay, height=2, width=20)
+        self.hotkey_label = tk.Label(self.root, text="Hotkey: {}".format(self.hotkey.char), height=2)
+        self.delay_label = tk.Label(self.root, text="Current Delay: {}".format(self.delay), height=2)
+        self.exit_button = tk.Button(self.root, text="Exit", command=self.exit_program, height=2, width=20)
 
-        # Place GUI components using grid layout
+        
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_rowconfigure(2, weight=1)
+        self.root.grid_rowconfigure(3, weight=1)
+        self.root.grid_rowconfigure(4, weight=1)
+        self.root.grid_rowconfigure(5, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
         self.start_stop_button.grid(row=0, column=0, pady=10)
         self.change_hotkey_button.grid(row=1, column=0, pady=5)
         self.change_delay_button.grid(row=2, column=0, pady=5)
@@ -36,11 +44,11 @@ class AutoClickerApp:
         self.delay_label.grid(row=4, column=0, pady=5)
         self.exit_button.grid(row=5, column=0, pady=5)
 
-        # Create listener for hotkey
+       
         self.listener = Listener(on_press=self.on_key_press)
         self.listener.start()
 
-        # Auto-clicking state and thread
+        
         self.clicking = False
         self.auto_click_thread = None
 
@@ -90,7 +98,7 @@ class AutoClickerApp:
     def get_new_delay(self):
         try:
             new_delay = float(simpledialog.askstring("Change Delay", "Enter new delay (in seconds):"))
-            return max(0, new_delay)  # Ensure delay is non-negative
+            return max(0, new_delay)  
         except (ValueError, TypeError):
             return None
 
@@ -103,7 +111,7 @@ class AutoClickerApp:
 
 def main():
     root = tk.Tk()
-    app = AutoClickerApp(root)
+    app = SheepMedAutoClicker(root)
     root.mainloop()
 
 if __name__ == "__main__":
